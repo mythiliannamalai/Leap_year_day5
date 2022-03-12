7861b3da54ef4ff93a07463b652ae792ab605e7c
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 18:57:01 +0530

    leap_year

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Leap
+{
+    public void Calculation()
+    {
+        Console.WriteLine("enter the year");
+        int year = int.Parse(Console.ReadLine());
+        if (((year % 4 == 0) && (year % 100 == 0)) || (year % 400 == 0))
+        {
+            Console.WriteLine("{0} is a leap year", year); 
+        }
+        else
+        {
+            Console.WriteLine("{0} is not leap year", year);
+        }
+    }
+}
+class Result
+{
+    public static void Main(string[] args)
+    {
+        Leap leap = new Leap();
+        leap.Calculation();
+    }
+}
